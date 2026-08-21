0bb5e5b6eb563b837d8866e9d2c4ddb1999f15d9
Marre24 <Maximilian.ellnestam@gmail.com>
2026-08-21 14:38:15 +0200

    Refactor

--- js/money/moneyRepository.js
+++ js/money/moneyRepository.js
@@ -2,11 +2,16 @@ export function currentMoney() {
   return parseInt(localStorage.getItem("money")) || 0;
 }
 
-export function store(newMoney) {
-  localStorage.setItem("money", newMoney);
-}
-
 export function deductFromBalance(amount) {
   const newMoney = currentMoney() - amount;
   store(newMoney);
+}
+
+export function addMoneyToBalance(amount) {
+  const newMoney = currentMoney() + amount;
+  store(newMoney);
+}
+
+function store(newMoney) {
+  localStorage.setItem("money", newMoney);
 }
--- js/money/moneyService.js
+++ js/money/moneyService.js
@@ -1,22 +1,19 @@
 import { CoinSide } from "../coin/coinFlipper.js";
 import { showMoneyRemovedEffect } from "../vfx/visualEffects.js";
 import { currentMoney } from "./moneyRepository.js";
-import { store } from "./moneyRepository.js";
+import { addMoneyToBalance } from "./moneyRepository.js";
 import { deductFromBalance } from "./moneyRepository.js";
 
 const moneyLabel = document.getElementById("moneyLabel");
 const moneyForHeads = 2;
 
 export function updateMoney(streak) {
-  const cm = currentMoney();
-
   if (streak === -1) {
     return;
   }
 
   const moneyForFlip = moneyForHeads ** streak;
-  const newMoney = cm + moneyForFlip;
-  store(newMoney);
+  addMoneyToBalance(moneyForFlip);
   updateMoneyDisplay();
   return moneyForFlip;
 }
